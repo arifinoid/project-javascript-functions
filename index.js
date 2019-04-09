1f88ee5fef56499ad73f4677bd32cea73fb03d0f
Rohmad Arifin <rohmadarifin007@gmail.com>
2019-04-10 03:04:31 +0700

    change match probability result into integer number, add percentage result

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const result = () => {
   const getName = document.getElementById("name").value;
   const getPartner = document.getElementById("partner").value;
-  const calculate = Math.random();
+  const calculate = Math.floor(Math.random() * 100) + 1;
   const output =
-    calculate > 0.5 ? "you are the ideal partner" : "better get divorced";
+    calculate > 75
+      ? "You are the ideal partner for her/him"
+      : "Find another one. There are still many boys/girls out there";
 
-  console.log(`${getName} and ${getPartner} get ${calculate} probability`);
+  console.log(`${getName} and ${getPartner} get ${calculate}% probability`);
   console.log(output);
   alert(`${output}. go to chrome DevTools Console for more info.`);
 };
